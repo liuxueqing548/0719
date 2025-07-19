dba915810d8efe772e76d1e73e217835bddadffc
liuxueqing548 <liuxueqing548@163.com>
2025-07-19 19:13:05 +0800

    删除

--- components/AIWidget.js
+++
@@ -1,9 +0,0 @@
-export default function AIWidget() {
-  return (
-    <div className="fixed bottom-6 right-6">
-      <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full shadow-lg">
-        🤖 AI助手
-      </button>
-    </div>
-  )
-}
